0bd2bae7d64068d465a106d50919c256ce347532
NewHorizon2023 <lxnirl@gmail.com>
2024-07-20 15:14:55 +0100

    test commit

---
+++ day01-MybatisPlus/material/mp-demo/src/main/java/com/itheima/mp/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.itheima.mp.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.itheima.mp.domain.po.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+//    void saveUser(User user);
+//
+//    void deleteUser(Long id);
+//
+//    void updateUser(User user);
+//
+//    User queryUserById(@Param("id") Long id);
+//
+//    List<User> queryUserByIds(@Param("ids") List<Long> ids);
+}
